0452454b6d33638a3e532e3ea23732659bb28406
Mohammad Jafari <jafarim@gmail.com>
2020-06-29 00:02:35 -0700

    support resource redaction.

--- tests/consent-enforcement.test.js
+++ tests/consent-enforcement.test.js
@@ -1,6 +1,7 @@
 const nock = require("nock");
 const request = require("supertest");
 const app = require("../app");
+const _ = require("lodash");
 
 const FHIR_SERVER_BASE =
   process.env.FHIR_SERVER_BASE || "https://mock-fhir-server/base";
@@ -93,7 +94,12 @@ afterEach(() => {
 it("should fetch a resource if consent permits", async () => {
   expect.assertions(2);
   const patient = require("./fixtures/patient.json");
-  const medication = require("./fixtures/medication-statement.json");
+
+  const medication = _.cloneDeep(
+    require("./fixtures/medication-statement.json")
+  );
+  medication.meta = undefined;
+
   MOCK_FHIR_SERVER.get("/Patient/1").reply(200, patient);
   MOCK_FHIR_SERVER.get("/MedicationStatement/1").reply(200, medication);
 
@@ -115,6 +121,22 @@ it("should send 403 if consent denies", async () => {
   MOCK_FHIR_SERVER.get("/MedicationStatement/1").reply(200, medication);
 
   MOCK_CDS.post(CDS_ENDPOINT).reply(200, CDS_DENY_RESPONSE);
+
+  const res = await request(app)
+    .get("/MedicationStatement/1")
+    .set("content-type", "application/json");
+
+  expect(res.status).toEqual(403);
+});
+
+it("should send 403 if consent obligation requires redaction", async () => {
+  expect.assertions(1);
+  const patient = require("./fixtures/patient.json");
+  const medication = require("./fixtures/medication-statement.json");
+  MOCK_FHIR_SERVER.get("/Patient/1").reply(200, patient);
+  MOCK_FHIR_SERVER.get("/MedicationStatement/1").reply(200, medication);
+
+  MOCK_CDS.post(CDS_ENDPOINT).reply(200, CDS_PERMIT_RESPONSE);
 
   const res = await request(app)
     .get("/MedicationStatement/1")
