9f79b1a7389e173e8b17044ef982d815e056ba15
Mohammad Jafari <jafarim@gmail.com>
2020-08-31 10:44:43 -0700

    content class obligations

--- tests/consent-enforcement.test.js
+++ tests/consent-enforcement.test.js
@@ -46,6 +46,39 @@ const CDS_PERMIT_RESPONSE = {
   ]
 };
 
+const CDS_PERMIT_RESPONSE_CONTENT_CLASS_OBLIGATION = {
+  cards: [
+    {
+      summary: "CONSENT_PERMIT",
+      detail: "There is a patient consent permitting this action.",
+      indicator: "info",
+      source: {
+        label: "Sample",
+        url: "https://sample-cdms.org"
+      },
+      extension: {
+        decision: "CONSENT_PERMIT",
+        obligations: [
+          {
+            id: {
+              system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+              code: "REDACT"
+            },
+            parameters: {
+              codes: [
+                {
+                  system: "http://hl7.org/fhir/resource-types",
+                  code: "MedicationStatement"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
+
 const CDS_DENY_RESPONSE = {
   cards: [
     {
@@ -187,4 +220,31 @@ it("should fetch a bundle and return only the resources which the consent permit
   expect(res.status).toEqual(200);
   expect(res.body.entry.length).toEqual(1);
   expect(res.body.entry[0].resource.id).toEqual("1");
+});
+
+it("should fetch a bundle and return only the resources which the consent permits and not to be redacted based on resource type redaction obligations", async () => {
+  expect.assertions(2);
+  const patient1 = require("./fixtures/patient.json");
+  const patient2 = require("./fixtures/patient-second.json");
+  const medicationBundle = require("./fixtures/medication-statement-bundle.json");
+  MOCK_FHIR_SERVER.get("/Patient/1").reply(200, patient1);
+  MOCK_FHIR_SERVER.get("/Patient/2").reply(200, patient2);
+  MOCK_FHIR_SERVER.get("/MedicationStatement").reply(200, medicationBundle);
+
+  MOCK_CDS.post(CDS_ENDPOINT).reply(
+    200,
+    CDS_PERMIT_RESPONSE_CONTENT_CLASS_OBLIGATION
+  );
+  MOCK_CDS.post(CDS_ENDPOINT).reply(
+    200,
+    CDS_PERMIT_RESPONSE_CONTENT_CLASS_OBLIGATION
+  );
+
+  const res = await request(app)
+    .get("/MedicationStatement")
+    .set("content-type", "application/json")
+    .set("Authorization", `Bearer ${JWT}`);
+
+  expect(res.status).toEqual(200);
+  expect(res.body.entry.length).toEqual(0);
 });
